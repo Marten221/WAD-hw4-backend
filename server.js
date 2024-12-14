3760c8552454165f2695057ff43d8323d94479f2
Marten221 <marten.ojasaar@ut.ee>
2024-12-14 18:29:00 +0200

    Created needed functions, to make authentication work

--- server.js
+++ server.js
@@ -1,32 +1,106 @@
 const express = require('express');
+const pool = require('./database');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+const port = process.env.PORT || 3000;
+
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const secret = "13887c9e1458c7555eb5813e24967f9f783da0cf5373e5deaf9a94020560bc91a9a8244dfade29da02a3fb7a429e066597d815db16990b1eb9d5dea34f4dcc97bd53fd010d5797de7e39864174cff25571b5b7a4891098456bdf287aa36857a46a2bb02f80cdd3c4d85335ab030344762ae33d08d929701717517d26c12089b1f116029181d616d5b26a05ca619e53301f473bfdd5a91239ff58bf8cce2b2e5ccb670f76b5883fa53750e25796526931e92f2dd7482b87c8456a1818076d31bfbee99510f9576906c728ff382b1b3cf63d5a4194ef14eef6d005d71691f6657cf0c95443cbc5c5632d000ead4b85e4ffc2fb0896bb2c2d7579654970ed9a8728";
+const maxAge = 60 * 60;
+
+const generateJWT = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: maxAge });
+};
+
 // listen for requests on port 3000
-app.listen(3001);
-
-/* app.get() is used to respond to Get requests, and it takes two arguments: 
-1- arg1: represents what path/url you want to listen to (e.g., '/' listens to index path)
-2- arg2: represents a function that takes in request and response objects */
-
-app.get('/', (req, res) => {
-    // res.send can be used to send text and HTML snippets
-    //res.send('</h1>First HTML response message! </h1>');
-
-    /* res.sendFile() is a method that can be used to send files as its name indicates
-    However, it takes the absolute not the relative path to the file. Therefore, you need to specify what is the root directory.
-    To avoid this confusion, you can use  "__dirname"*/
-    res.sendFile('./views/index.html', { root: __dirname });
-    console.log(__dirname);
-});
-
-// do not write in the URL address bar the extension of the page. For example, writing (http://localhost:3000/posts.html) will direct you to the 404 page
-app.get('/posts', (req, res) => {
-    res.sendFile('./views/posts.html', { root: __dirname });
+app.listen(port, () => {
+    console.log("Server is listening to port: " + port)
 });
 
 
 
-// We will discuss this method next week, when we speak about Middlewares
-app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+signUpQuery = "INSERT INTO users(email,password) values ($1, $2) RETURNING*"
+app.post('/auth/signup', async (req, res) => {
+    try {
+        console.log("Signup request arrived!")
+        const { email, password } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const bcryptPassword = await bcrypt.hash(password, salt);
+        const authUser = await pool.query(signUpQuery, [email, bcryptPassword]);
+
+        console.log(authUser.rows[0]);
+        const token = await generateJWT(authUser.rows[0].id);
+        res
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
+            .json({ user_id: authUser.rows[0].id })
+            .send;
+    } catch (error) {
+        res.status(401).json({ error: error.message })
+    }
+})
+
+app.get('/auth/authenticate', async (req, res) => {
+    console.log("Auth req arrived!")
+    const token = req.cookies.jwt;
+    console.log("token: ", token);
+
+    let authenticated = false;
+    try {
+        if (token) {
+            await jwt.verify(token, secret, (err) => {
+                if (err) {
+                    console.log('Token not verified: ', err.message)
+                    res.send({ "authenticated": authenticated })
+                } else {
+                    console.log("User successfully authenticated")
+                    authenticated = true;
+                    res.send({ "authenticated": authenticated });
+                }
+            })
+        } else {
+            console.log("Token does not exist");
+            res.send({ "authenticated": authenticated })
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(400).send(err.message)
+    }
+});
+
+getCredentialsQuery = "SELECT * FROM users WHERE email = $1"
+app.post('/auth/login', async (req, res) => {
+    try {
+        console.log("Login request arrived!")
+        const { email, password } = req.body
+
+        const user = await pool.query(getCredentialsQuery, [email]);
+        if (user.rows.length === 0) return res.status(401).json({ error: "User is not registered" })
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
+
+        const token = await generateJWT(user.rows[0].id);
+        res 
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true})
+            .json({ user_id: user.rows[0]. id })
+            .send;
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+});
+
+app.get('/auth/logout', (req, res) => {
+    console.log("Logout req arrived!")
+    res.status(202).clearCookie("jwt").json({"Msg": "cookie cleared"}).send
 });
