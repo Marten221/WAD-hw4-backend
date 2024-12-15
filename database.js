f28681bee2c8c05b4eaa62ca944349f13ba82374
nurmikma <143510138+nurmikma@users.noreply.github.com>
2024-12-15 22:49:03 +0200

    correct password for buddies

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "superuser", //add your password
+    password: "pgAdmin", //add your password
     database: "testWad",
     host: "localhost",
     port: "5432"
